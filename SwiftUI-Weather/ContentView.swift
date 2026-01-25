2d5b41da74450a29ea573988bc9bf742eb75f06c
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2026-01-25 14:41:30 +0500

    feat: Make location text and image view layouts

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -8,19 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Subviews
+    
     var body: some View {
-        Form {
-            VStack {
-                Text("SomeForm")
-                    .padding()
-                Text("SomeForm")
-                    .padding()
-                    .background()
-            }
+        ZStack {
+            backgroundView
+            weatherView
         }
     }
+    
+    var weatherView: some View {
+        VStack(
+            alignment: .center,
+            spacing: 32
+        ) {
+            locationTextView
+            VStack(spacing: 16) {
+                locationImageView
+                degreeTextView
+            }
+            Spacer()
+        }
+    }
+    
+    var locationTextView: some View {
+        Text("Cupertino, CA")
+            .font(.system(.title, design: .monospaced, weight: .semibold))
+            .foregroundColor(.white)
+            .padding(.top, 16)
+    }
+    
+    var locationImageView: some View {
+        Image(systemName: "cloud.sun.fill")
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 180, height: 140)
+    }
+    
+    var degreeTextView: some View {
+        Text("76°")
+            .font(.system(size: 64, weight: .bold, design: .monospaced))
+            .foregroundColor(.white)
+    }
+    
+    var backgroundView: some View {
+        LinearGradient(
+            gradient: .init(colors: [.blue, .white]),
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+        .ignoresSafeArea(.all)
+    }
 }
-
 
 #Preview {
     ContentView()
